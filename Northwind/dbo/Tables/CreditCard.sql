e83f717e1b1f107b6d3de0f63061497c805b8470
Yauheniya Strelkova <Yauheniya_Strelkova@epam.com>
2020-03-12 10:35:41 +0300

    Add project files.

---
+++ Northwind/dbo/Tables/CreditCard.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[CreditCard]
+(
+	[CreditCardId] INT NOT NULL PRIMARY KEY, 
+    [CardHolder] NCHAR(50) NOT NULL, 
+    [ExpirationDate] DATE NOT NULL, 
+    [EmployeeID] INT NOT NULL, 
+    CONSTRAINT [FK_CreditCard_Employees] FOREIGN KEY ([EmployeeID]) REFERENCES [Employees]([EmployeeID])
+)
